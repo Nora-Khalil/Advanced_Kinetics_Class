7e7d94871623e95967bfe3032632d60dccdec1ee
Nora Khalil <khalil.nor@northeastern.edu>
2023-12-05 15:10:45 -0500

    Needham core, NIST lib, NIST thermo

---
+++ project/models/RMG/Needham_core_NIST_lib_NIST_thermo/flame_speed_run_39-stoic.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=39_Need_NIST_kin_thermo
+#SBATCH --error=error_fc_39_stoic.slurm.log
+#SBATCH --output=output_fc_39_stoic.slurm.log
+##SBATCH --cpus-per-task=5
+##SBATCH --mem-per-cpu=8Gb
+##SBATCH --ntasks=1 
+##SBATCH --array=1
+#SBATCH --partition=short
+
+#source activate cantera_env
+python flame_speed_calc_39-stoic.py
+
